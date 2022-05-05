66126a350c1767ef3716b1729ca8f51abdec1930
Mateus Lanes <lanesd2010@gmail.com>
2022-05-04 22:05:05 -0400

    ok

--- appTodo/src/app/home/home.page.ts
+++ appTodo/src/app/home/home.page.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { AlertController,  ToastController} from '@ionic/angular';
-import { toastController } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -9,67 +7,6 @@ import { toastController } from '@ionic/core';
 })
 export class HomePage {
 
-  tarefas: any[] = [];
-  constructor(private alertCtrl: AlertController,  private toastCtrl: ToastController) {}
-    async showAdd() {
+  constructor() {}
 
-      const alert = await this.alertCtrl.create({
-      cssClass: 'my-custom-class',
-      header: 'O que você deseja fazer?',
-      inputs: [
-      {
-      name: 'tarefa1',
-      type: 'text',
-      placeholder: 'Digite o que deseja fazer.',
-      },
-      ],
-      buttons: [
-      {
-      text: 'Cancelar',
-      role: 'Cancel',
-      cssClass: 'secondary',
-      handler: () => {
-      console.log('Cancelado com sucesso!');
-      },
-      },
-      {
-        text: 'Adicionar',
-        handler: (form) => {
-        this.adicionarTarefa(form.tarefa1);
-        
-      },
-      
-    },
-    ],
-    });
-
-    
-    await alert.present();
-
-  }
-
-  async adicionarTarefa(novaTarefa: string){
-    if (novaTarefa.trim().length <1 ){
-      const toast = await this.toastCtrl.create({
-        message: 'Por favor, digite a tarefa!',
-
-        duration: 2000,
-
-        position: 'top',
-        
-
-      });
-      
-      const tarefa = { nome: novaTarefa, realizada: false};
-      this.tarefas.push(tarefa);
-
-      toast.present();
-      
-    }
-    return;
-  }
 }
-  function showAdd() {
-    throw new Error('Function not implemented.');
-  }
-
